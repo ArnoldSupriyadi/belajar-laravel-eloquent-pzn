524295223e7aafa9d963f3cc411448ecad72a382
Arnold Supriyadi <arnoldsupriyadi@gmail.com>
2023-11-07 10:22:14 +0700

    baru sampe video 189 UUID

--- tests/Feature/CategoryTest.php
+++ tests/Feature/CategoryTest.php
@@ -9,6 +9,7 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 use function PHPUnit\Framework\assertEquals;
+use function PHPUnit\Framework\assertTrue;
 
 class CategoryTest extends TestCase
 {
@@ -70,4 +71,81 @@ class CategoryTest extends TestCase
 
         self::assertTrue($result);
     }
+
+    public function testSelect()
+    {
+        for($i = 0; $i < 5; $i++) {
+            $category = new Category();
+            $category->id = "$i";
+            $category->name = "Category $i";
+            $category->save();
+        }
+
+        $categories = Category::query()->whereNull("description")->get();
+        self::assertEquals(5, $categories->count());
+        $categories->each(function ($category){
+            self::assertNull($category->description);
+
+            $category->description = "Updated";
+            $category->update();
+        });
+    }
+
+    public function testUpdateMany()
+    {
+        $categories = [];
+        for($i = 0; $i < 10; $i++) {
+            $categories[] = [
+                'id' => "ID $i",
+                'name' => "Name $i"
+            ];
+        }
+
+        $result = Category::insert($categories);
+        assertTrue($result);
+
+        Category::whereNull("description")->update([
+            "description" => "Updated"
+        ]);
+
+        $total = Category::where("description", "=", "Updated")->count();
+        self::assertEquals(10, $total);
+    }
+
+    public function testDelete()
+    {
+        $this->seed(CategorySeeder::class);
+
+        $category = Category::query()->find("FOOD");
+        $result = $category->delete();
+
+        self::assertTrue($result);
+
+        $total = Category::query()->count();
+        self::assertEquals(0, $total);
+    }
+
+    public function testDeleteMany()
+    {
+        $categories = [];
+        for($i = 0; $i < 10; $i++) {
+            $categories[] = [
+                'id' => "ID $i",
+                'name' => "Name $i"
+            ];
+        }
+
+        $result = Category::insert($categories);
+        self::assertTrue($result);
+
+        $total = Category::count();
+        assertEquals(10, $total);
+
+        Category::whereNull("description")->delete();
+
+        $total = Category::count();
+        assertEquals(0, $total);
+    }
+
+
 }

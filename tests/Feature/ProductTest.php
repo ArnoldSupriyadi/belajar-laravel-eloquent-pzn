37ce2ff21cecabd52c787fcb202b4be491fefc5c
Arnold Supriyadi <arnoldsupriyadi@gmail.com>
2023-12-13 11:53:39 +0700

    221. Baru sampe video factory

--- tests/Feature/ProductTest.php
+++ tests/Feature/ProductTest.php
@@ -123,4 +123,27 @@ class ProductTest extends TestCase
         self::assertNotNull($products);
         self::assertNotNull("2", $products[0]->id);
     }
+
+    public function testSerialization()
+    {
+        $this->seed([CategorySeeder::class, ProductSeeder::class]);
+
+        $products = Product::query()->get();
+        self::assertCount(2, $products);
+
+        $json = $products->toJson(JSON_PRETTY_PRINT);
+        Log::info($json);
+    }
+
+    public function testSerializationRelation()
+    {
+        $this->seed([CategorySeeder::class, ProductSeeder::class, ImageSeeder::class]);
+
+        $products = Product::query()->get();
+        $products->load(["category", "image"]);
+        self::assertCount(2, $products);
+
+        $json = $products->toJson(JSON_PRETTY_PRINT);
+        Log::info($json);
+    }
 }
